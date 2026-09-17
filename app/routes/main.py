81dcfce39e0108e33180d1207db289cb47cf9f2b
Cao Minh Nhựt <caotrinhan2019@gmail.com>
2026-09-17 15:20:40 +0700

    edit thanh dang xem 2

--- app/routes/main.py
+++ app/routes/main.py
@@ -87,8 +87,8 @@ def ping_online():
         
     if current_user.is_authenticated:
         identifier = f"user_{current_user.id}"
-        display_name = f"{current_user.full_name} ({current_user.email} - {current_user.unit})"
-    else:
+        display_name = current_user.email  # <--- Sửa ở đây thành chỉ lấy email của user
+    else:   
         ip = request.headers.get('X-Forwarded-For', request.remote_addr)
         if ip and ',' in ip:
             ip = ip.split(',')[0].strip()
